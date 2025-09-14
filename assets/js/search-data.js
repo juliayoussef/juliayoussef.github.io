cd86e5cc66697c91d95bac5d4f231c776e43f8e9
jhyoussef <107482498+jhyoussef@users.noreply.github.com>
2025-09-14 21:55:22 +0000

    Deploying to gh-pages from @ jhyoussef/jhyoussef.github.io@4402bee9acd610f986fcb107c0c1026e6abb9eb2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,13 +46,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-my-first-announcement-sparkles-smile",
+          title: 'My first announcement! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-learning-anatomy",
           title: 'Learning Anatomy',
