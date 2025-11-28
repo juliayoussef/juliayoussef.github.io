f1ba09e62cb7491756fa5c68645df0807a21ebb7
jhyoussef <107482498+jhyoussef@users.noreply.github.com>
2025-11-28 07:21:38 +0000

    Deploying to gh-pages from @ jhyoussef/jhyoussef.github.io@09fc6f1cd2932c73238a14d8d0426d3ef6c2034d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-arts",
+          title: "arts",
+          description: "my work",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/arts/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
