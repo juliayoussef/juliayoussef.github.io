f1a1573b50197ab0e2b81774017ddfe9d713f3a2
juliayoussef <107482498+juliayoussef@users.noreply.github.com>
2026-02-08 19:12:19 +0000

    Deploying to gh-pages from @ juliayoussef/juliayoussef.github.io@8d4db10020b8e2a673c667ac0e2885013fdd036a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jhyoussef/jhyoussef.github.io",
+    "data-repo": "juliayoussef/juliayoussef.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
